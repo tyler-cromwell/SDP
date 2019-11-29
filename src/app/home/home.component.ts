5de7a40463bcf6628edf5210812829f2d89bea0b
Tyler Cromwell <tjc6185@gmail.com>
2019-11-28 21:23:48 -0500

    [home]: Added base modal for Advanced Search

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as M from "materialize-css/dist/js/materialize";
+
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor() {
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.modal');
+      var instances = M.Modal.init(elems);
+    });
+  }
   ngOnInit() {}
 }
