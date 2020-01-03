b7836e339b5553a127dbfd467ba2217470fd5895
Tyler Cromwell <tjc6185@gmail.com>
2020-01-03 14:01:24 -0500

    [refactor]: Eliminated browse page, consolidated with home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { BrowseComponent } from './browse/browse.component';
 import { CreateComponent } from './create/create.component';
 import { DetailComponent } from './detail/detail.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'browse', component: BrowseComponent},
   { path: 'browse/:id', component: DetailComponent},
   { path: 'create', component: CreateComponent}
 ];
@@ -21,7 +19,6 @@ const routes: Routes = [
 export class AppRoutingModule { }
 export const routingComponents = [
   HomeComponent,
-  BrowseComponent,
   CreateComponent,
   DetailComponent
 ]
