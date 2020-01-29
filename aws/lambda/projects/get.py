5d8c4b797f1be022ea8e5d679df31dfba66bb39b
Tyler Cromwell <tjc6185@gmail.com>
2020-01-29 17:15:19 -0500

    [fixes]: Fixed Lambda function for getting all projects

--- aws/lambda/projects/get.py
+++ aws/lambda/projects/get.py
@@ -5,4 +5,4 @@ import boto3
 
 def main(event, context):
     client = boto3.client('dynamodb')
-    return json.dumps(client.get_item(TableName='cse4940-projects'))
+    return json.dumps(client.scan(TableName='cse4940-projects'))
