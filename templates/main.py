11c064368d2bb155e3ab773525fe0937430ce005
Cristian Gutu <gutucristian27@gmail.com>
2020-02-11 21:23:00 -0500

    detect CloudFormation template PUT into S3 bucket and deploy stack

--- templates/main.py
+++ templates/main.py
@@ -1,12 +1,26 @@
+import string
+import random
 import boto3
-cf_client = boto3.client('cloudformation')
 
-# StackName: name of stack, can be dynamically generated, it must be unique
-# TemplateURL: the URL of the CloudFormation template that defines the user requested stack
-
-stack_result = cf_client.create_stack(
-    StackName='foobar', 
-    TemplateURL='https://naitsirc.s3.amazonaws.com/foo.yaml'
-)
-
-print(stack_result)
+def lambda_handler(event, context):
+  cf_client = boto3.client('cloudformation')
+  
+  location = event["Records"][0]["s3"]["bucket"]["name"]
+  key = event["Records"][0]["s3"]["object"]["key"]
+  url = "https://{}.s3.amazonaws.com/{}".format(location, key)
+  
+  print("location: {}".format(location))
+  print("key: {}".format(key))
+  print("url: {}".format(url))
+  
+  cf_client = boto3.client('cloudformation')
+  stack_result = cf_client.create_stack(
+    StackName = get_stack_name(), # name of stack, must be unique
+    TemplateURL = url # the URL of the CloudFormation template that defines the user requested stack
+  )
+  
+  print(stack_result)
+  
+def get_stack_name(length=20):
+  store = string.ascii_letters + string.digits
+  return random.choice(string.ascii_letters) + ''.join([random.choice(store) for i in range(length - 1)])
