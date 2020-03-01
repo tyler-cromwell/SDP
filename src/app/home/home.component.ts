e474b4138b046b2908aec90c9d41b8335268af85
Cristian Gutu <gutucristian27@gmail.com>
2020-03-01 17:49:10 -0500

    [api]: remove unnecessary json parse

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ import * as M from "materialize-css/dist/js/materialize";
   providers: [AWSClientService]
 })
 export class HomeComponent implements OnInit {
-  private projects = [];
+  private projects: any = [];
 
   constructor(private client: AWSClientService) {
     // document.addEventListener('DOMContentLoaded', function() {
@@ -19,14 +19,9 @@ export class HomeComponent implements OnInit {
     //   var instances = M.Modal.init(elems);
     // });
 
-    this.client.getProjects().subscribe(data => {
-        let items = JSON.parse(data)["Items"]
-
-        if (items.length) {
-          this.projects = items
-        }
-      }
-    )
+    this.client.getProjects().subscribe(data => {              
+      this.projects = data;
+    });
   }
 
   toggleModal() {
