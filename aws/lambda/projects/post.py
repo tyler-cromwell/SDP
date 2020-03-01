5cc66ffdc4a2d134ee3c758ec2ab03f251739e25
Cristian Gutu <gutucristian27@gmail.com>
2020-03-01 15:17:41 -0500

    [api]: add owner validation

--- aws/lambda/projects/post.py
+++ aws/lambda/projects/post.py
@@ -1,20 +1,32 @@
 import json
 import uuid
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
-# def isOwnerValid(email: str) -> bool:
-#     return True;
+def isOwnerValid(email: str) -> bool:
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('cse4940-users')
+    
+    result = table.query(
+        KeyConditionExpression=Key('email').eq(email)
+    )
+    
+    return len(result["Items"]) == 1
 
 def main(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('cse4940-projects')
-    print(json.dumps(event, indent=4, sort_keys=True))
     
     required = ['name', 'owner', 'description', 'version']
 
     if False in [k in event.keys() for k in required]:
         return {
             'error': 'Missing a required key'
+        }
+
+    if not isOwnerValid(event['owner']):
+        return {
+            'error': 'Owner does not exist'
         }
 
     result = table.put_item(
