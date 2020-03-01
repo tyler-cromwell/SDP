2b0e8651f7ebb49eaf30fa8aa2a0f6a19829e358
Cristian Gutu <gutucristian27@gmail.com>
2020-03-01 14:54:01 -0500

    [api]: clean up home component and place api call in ngOnInit hook

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,21 +13,16 @@ import * as M from "materialize-css/dist/js/materialize";
 export class HomeComponent implements OnInit {
   private projects = [];
 
-  constructor(private client: AWSClientService) {
-    document.addEventListener('DOMContentLoaded', function() {
-      var elems = document.querySelectorAll('.modal');
-      var instances = M.Modal.init(elems);
-    });
-
-    this.client.getProjects().subscribe(data => {
-        let items = JSON.parse(data)["Items"]
-
-        if (items.length) {
-          this.projects = items
-        }
-      }
-    )
+  constructor(private client: AWSClientService) { }
+  
+  ngOnInit() {    
+      document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.modal');
+        var instances = M.Modal.init(elems);
+      });
+    
+      this.client.getProjects().subscribe(data => {                         
+          this.projects = data          
+      });
   }
-
-  ngOnInit() { }
 }
