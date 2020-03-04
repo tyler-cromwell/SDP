817b0ed4a0c22f921fa2fe865e010f9db2d46a6a
Tyler Cromwell <tjc6185@gmail.com>
2020-03-04 12:24:53 -0500

    [infra]: Added functionality for adding an APIGateway REST API

--- aws/infra/main.py
+++ aws/infra/main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-import sys
 import argparse
+import sys
 
 import boto3
 import botocore
 
 import client
 import template
-from utils import read_credentials, get_random_name, stack_exists
+import utils
+
 
 if __name__ == '__main__':
     my_parser = argparse.ArgumentParser(
@@ -19,13 +20,13 @@ if __name__ == '__main__':
     my_parser.add_argument('-p', '--path', action='store', type=str,
                            required=True, help="path to AWS credentials file")
     my_parser.add_argument('-sn', '--stack-name', action='store', type=str,
-                           required=False, default=get_random_name(), help="name of CloudFormation stack")
+                           required=False, default=utils.get_random_name(), help="name of CloudFormation stack")
     my_parser.add_argument('-ec2k', '--ec2-key-pair', action='store', type=str,
                            required=False, default=DEFAULT_KEY_PAIR, help="name of EC2 key pair")
     my_parser.add_argument('-ec2n', '--ec2-name', action='store', type=str,
-                           required=False, default=get_random_name(), help="name of EC2 instance")
+                           required=False, default=utils.get_random_name(), help="name of EC2 instance")
     my_parser.add_argument('-lfn', '--lambda-function-name', action='store', type=str,
-                           required=False, default=get_random_name(), help="name of lambda function")
+                           required=False, default=utils.get_random_name(), help="name of lambda function")
     my_parser.add_argument('-lfnp', '--lambda-function-path', action='store',
                            type=str, required=False, help="path of lambda function")
 
@@ -37,8 +38,12 @@ if __name__ == '__main__':
     EC2_NAME = args.ec2_name
     LAMBDA_FUNCTION_NAME = args.lambda_function_name
     LAMBDA_FUNCTION_PATH = args.lambda_function_path
+    API_NAME = 'FakeAPI'
+    API_KEY_NAME = 'FakeAPIKey'
+    API_USAGE_PLAN_NAME = 'FakeAPIUsagePlan'
+    API_STAGE_NAME = 'development'
 
-    ACCESS, SECRET = read_credentials(PATH)
+    ACCESS, SECRET = utils.read_credentials(PATH)
 
     session = boto3.session.Session(
         aws_access_key_id=ACCESS,
@@ -46,17 +51,49 @@ if __name__ == '__main__':
     )
 
     template = template.Template()
-    # template.add_lambda_function(LAMBDA_FUNCTION_NAME, LAMBDA_FUNCTION_PATH)
+    """
+    template.add_lambda_function(
+        name=LAMBDA_FUNCTION_NAME,
+        filename=LAMBDA_FUNCTION_PATH,
+        managed_policies=[
+            'arn:aws:iam::aws:policy/AWSCloudFormationReadOnlyAccess',
+            'arn:aws:iam::aws:policy/AmazonDynamoDBReadOnlyAccess',
+            'arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess'
+        ]
+    )
     template.add_ec2_instance(
         name=EC2_NAME,
         instance_type='t2.micro',
         key_name=EC2_KEY_PAIR,
         machine_image='ami-04b9e92b5572fa0d1'
     )
+    template.add_apigateway_api(
+        name=API_NAME
+    )
+    template.add_apigateway_method(
+        name='FakeAPIMethod',
+        method_type='GET',
+        resource='RootResourceId',
+        api_name=API_NAME
+    )
+    template.add_apigateway_deployment(
+        name='InitialDeployment',
+        api_name=API_NAME,
+        stage_name=API_STAGE_NAME,
+        methods=['FakeAPIMethod'+'GET']
+    )
+    template.add_apigateway_usage_plan(
+        name=API_USAGE_PLAN_NAME,
+        api_name=API_NAME,
+        key_name=API_KEY_NAME,
+        stage_name=API_STAGE_NAME,
+        deployment_name='InitialDeployment'
+    )
+    """
 
     cfclient = session.client('cloudformation')
 
-    if stack_exists(cfclient, STACK_NAME):
+    if utils.stack_exists(cfclient, STACK_NAME):
         print('Stack "{}" exists!'.format(STACK_NAME))
     else:
         response = client.create_stack(
--- aws/infra/template.py
+++ aws/infra/template.py
@@ -24,6 +24,98 @@ class Template:
         return {'Ref': name}
 
 
+    def add_apigateway_api(self, name):
+        self.json['Resources'][name] = {
+            'Type': 'AWS::ApiGateway::RestApi',
+            'Properties': {
+                'ApiKeySourceType': 'HEADER',
+                'Name': name
+            }
+        }
+
+
+    def add_apigateway_deployment(self, name, api_name, stage_name, methods):
+        self.json['Resources'][name] = {
+            'DependsOn': methods,
+            'Type': 'AWS::ApiGateway::Deployment',
+            'Properties': {
+                'RestApiId': self._Ref(api_name),
+                'StageName': stage_name
+            }
+        }
+
+
+    def add_apigateway_usage_plan(self, name, api_name, key_name, stage_name, deployment_name):
+        # Create the API Usage Plan and associate with a Stage.
+        self.json['Resources'][name] = {
+            'DependsOn': deployment_name,
+            'Type': 'AWS::ApiGateway::UsagePlan',
+            'Properties': {
+                'ApiStages': [
+                    {
+                        'ApiId': self._Ref(api_name),
+                        'Stage': stage_name,
+                    }
+                ],
+                'UsagePlanName': name
+            }
+        }
+
+        # Create an API key for the Usage Plan.
+        self.json['Resources'][key_name] = {
+            'DependsOn': name,
+            'Type': 'AWS::ApiGateway::ApiKey',
+            'Properties': {
+                'Enabled': True,
+                'Name': key_name,
+                'StageKeys': [
+                    {
+                        'RestApiId': self._Ref(api_name),
+                        'StageName': stage_name
+                    }
+                ]
+            }
+        }
+
+        # Associates the previously created API key with the Usage Plan.
+        self.json['Resources'][name+'UsagePlanKey'] = {
+            'DependsOn': key_name,
+            'Type': 'AWS::ApiGateway::UsagePlanKey',
+            'Properties': {
+                'KeyId': self._Ref(key_name),
+                'KeyType': 'API_KEY',
+                'UsagePlanId': self._Ref(name)
+            }
+        }
+
+
+    def add_apigateway_resource(self, name):
+        self.json['Resources'][name] = {
+            'Type': 'AWS::ApiGateway::Resource',
+            'Properties': {
+            }
+        }
+
+        return {}
+
+
+    def add_apigateway_method(self, name, method_type, resource, api_name, require_key=True):
+        self.json['Resources'][name+method_type] = {
+            'DependsOn': api_name,
+            'Type': 'AWS::ApiGateway::Method',
+            'Properties': {
+                'ApiKeyRequired': require_key,
+                'AuthorizationType': 'NONE',
+                'HttpMethod': method_type,
+                'Integration': {
+                    'Type': 'MOCK'
+                },
+                'ResourceId': self._FnGetAtt(api_name, resource),
+                'RestApiId': self._Ref(api_name)
+            }
+        }
+
+
     def add_ec2_instance(self, name, instance_type, key_name, machine_image, user_data=''):
         if key_name not in self.keys:
             self.keys.append(key_name)
@@ -87,7 +179,7 @@ class Template:
         }
 
 
-    def add_lambda_function(self, name, filename, create_role=True):
+    def add_lambda_function(self, name, filename, create_role=True, managed_policies=[]):
         with open(filename) as fp:
             code = fp.read()
 
@@ -96,9 +188,7 @@ class Template:
         if create_role:
             self.add_iam_role(
                 name=rolename,
-                managed_policies=[
-                    'arn:aws:iam::aws:policy/AmazonEC2FullAccess'
-                ],
+                managed_policies=managed_policies,
                 assume_policy={
                     'Version': '2012-10-17',
                     'Statement': [
