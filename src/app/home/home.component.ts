e01eadcb7b5efce678449769b91f4e60d9c59d8c
Cristian Gutu <gutucristian27@gmail.com>
2020-03-13 18:43:00 -0400

    [project-details-page]: add mock data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,20 @@ import * as M from "materialize-css/dist/js/materialize";
   providers: [AWSClientService]
 })
 export class HomeComponent implements OnInit {
-  private projects: any = [];
+  private projects: any = [
+    {
+      id: "sdf231as",
+      name: "FooBar",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      version: "1.0.0"
+    },
+    {
+      id: "fasf23ds2",
+      name: "BarFooBar",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+      version: "1.4.2"
+    }
+  ];
 
   constructor(private client: AWSClientService) { }
   
