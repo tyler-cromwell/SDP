90062009990611449105c39124c43db7d5e7e159
Cristian Gutu <gutucristian27@gmail.com>
2020-03-13 23:03:41 -0400

    [project-details-page]: add EventEmitter for ec2 creation

--- src/app/ec2/ec2.component.ts
+++ src/app/ec2/ec2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter, Input } from '@angular/core';
 import * as M from "materialize-css/dist/js/materialize";
 import { NgForm } from '@angular/forms';
 import { AWSClientService } from 'src/awsclient.service';
@@ -10,6 +10,8 @@ import { Template } from 'src/template'
   styleUrls: ['./ec2.component.css']
 })
 export class Ec2Component implements OnInit {
+  @Input() projectId: string = "qwe12re1";
+  @Output() ec2Created: EventEmitter<any> = new EventEmitter();
   @ViewChild('instanceTypeSelect', {static:true}) instanceTypeSelect: ElementRef;
   @ViewChild('machineImageSelect', {static:true}) machineImageSelect: ElementRef;
 
@@ -36,17 +38,22 @@ export class Ec2Component implements OnInit {
 
   onSubmit(form: NgForm) {
     let {name, instanceType, keyName, machineImage} = form.value;   
-    console.log(form)
-    console.log("name = " + name) 
-    console.log("instance type = " + instanceType) 
-    console.log("key name = " + keyName) 
-    console.log("machine Image = " + machineImage) 
+    console.log("name = " + name);
+    console.log("instance type = " + instanceType);
+    console.log("key name = " + keyName);
+    console.log("machine Image = " + machineImage);
     // let template: Template = new Template();
     // template.addEC2Instance(name, instanceType, keyName, machineImage);
     // this.client.createStack(this.randomString(), template).subscribe(
     //   data => {
-
     //   }
     // );
+    this.ec2Created.emit({
+      projectId: this.projectId,
+      name,
+      instanceType,
+      machineImage,
+      keyName
+    });
   }
 }
