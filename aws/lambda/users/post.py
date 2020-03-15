4ef75752ce4c3e5da4f9e7951bbdd49cadadeacb
Cristian Gutu <gutucristian27@gmail.com>
2020-03-15 13:26:10 -0400

    [api-cristian]: add required id and reformat

--- aws/lambda/users/post.py
+++ aws/lambda/users/post.py
@@ -1,24 +1,27 @@
 import json
 import boto3
+import uuid
+
 
 def main(event, context):
-  required = ['firstName', 'lastName', 'email', 'role']
+    required = ['firstName', 'lastName', 'email', 'role']
 
-  if False in [k in event.keys() for k in required]:
-    return {
-      'error': 'Missing a required key'
-    }
-  
-  dynamodb = boto3.resource('dynamodb')
-  table = dynamodb.Table('cse4940-users')
+    if False in [k in event.keys() for k in required]:
+        return {
+            'error': 'Missing a required key'
+        }
 
-  result = table.put_item(
-    Item={
-      'firstName': event['firstName'],
-      'lastName': event['lastName'],
-      'email': event['email'],
-      'role': event['role']
-    }
-  )
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('UsersTable')
 
-  return result
+    result = table.put_item(
+        Item={
+            'id': str(uuid.uuid4()),
+            'firstName': event['firstName'],
+            'lastName': event['lastName'],
+            'email': event['email'],
+            'role': event['role']
+        }
+    )
+
+    return result
