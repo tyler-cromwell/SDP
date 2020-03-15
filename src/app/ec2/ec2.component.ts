b22130a58391ed793a53f0e9a092f4ede8ead7dc
Cristian Gutu <gutucristian27@gmail.com>
2020-03-14 20:18:11 -0400

    [project-details-page]: add loading spinner

--- src/app/ec2/ec2.component.ts
+++ src/app/ec2/ec2.component.ts
@@ -13,20 +13,25 @@ export class Ec2Component implements OnInit {
   @Input() projectId: string = "qwe12re1";
   @Output() ec2Created: EventEmitter<any> = new EventEmitter();
   @ViewChild('instanceTypeSelect', {static:true}) instanceTypeSelect: ElementRef;
-  @ViewChild('machineImageSelect', {static:true}) machineImageSelect: ElementRef;
+  @ViewChild('machineImageSelect', {static:true}) machineImageSelect: ElementRef;  
 
   private ec2Name: string = this.randomString();
   private ec2KeyPair: string = "OurEC2Keypair01";
   private instanceTypes: string[] = ["t2.micro"];
   private machineImages: string[] = ["ami-0e38b48473ea57778"];
+  public isLoading: boolean;
 
   randomString(): string {
     return Math.random().toString(36).substring(7);
   }
 
-  constructor(private client: AWSClientService) { }
+  constructor(private client: AWSClientService) { 
+    this.isLoading = false;
+  }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    
+  }
 
   ngAfterViewInit() {
     M.FormSelect.init(this.instanceTypeSelect.nativeElement, {});    
@@ -38,22 +43,23 @@ export class Ec2Component implements OnInit {
 
   onSubmit(form: NgForm) {
     let {name, instanceType, keyName, machineImage} = form.value;   
-    console.log("name = " + name);
-    console.log("instance type = " + instanceType);
-    console.log("key name = " + keyName);
-    console.log("machine Image = " + machineImage);
     // let template: Template = new Template();
     // template.addEC2Instance(name, instanceType, keyName, machineImage);
     // this.client.createStack(this.randomString(), template).subscribe(
     //   data => {
     //   }
     // );
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
     this.ec2Created.emit({
       projectId: this.projectId,
       name,
       instanceType,
       machineImage,
-      keyName
+      keyName,
+      isLive: true
     });
   }
 }
