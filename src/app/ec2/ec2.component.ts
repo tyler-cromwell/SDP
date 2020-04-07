f3c9f7fb827c4d101a5a77d6e6190368dafd8408
Cristian Gutu <gutucristian27@gmail.com>
2020-04-07 00:32:18 -0400

    [create-ec2-comp]: added RSA private key pair download functionality

--- src/app/ec2/ec2.component.ts
+++ src/app/ec2/ec2.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter, Input }
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 import * as M from "materialize-css/dist/js/materialize";
-import { AWSClientService } from 'src/awsclient.service';
+import { AWSClientService } from 'src/services/awsclient.service';
+import { NotificationService } from 'src/services/notification.service';
 import { Project } from 'src/models/Project';
 
 @Component({
@@ -22,16 +23,31 @@ export class Ec2Component implements OnInit {
   private createForm: FormGroup;
   private instanceTypes: string[] = ["t2.micro"];
   private machineImages: string[] = ["ami-0e38b48473ea57778"];
-  private createNewKeyPair: Boolean = false;
+  private createNewKeyPair: Boolean = true;
   private initialFormValues = null;
+  private isLoading: Boolean = false;
 
-  constructor(private client: AWSClientService) { }
+  private receivedRSAPrivateKeyPair: Boolean = false;
+  private RSAPrivateKey: string = null;
+  private keyName: string = null;
+
+  constructor(private client: AWSClientService, private notifications: NotificationService) {
+    this.notifications.EC2Created.subscribe(data => {      
+      this.isLoading = false;      
+    });
+    this.notifications.RSAPrivateKey.subscribe(data => {      
+      this.receivedRSAPrivateKeyPair = true;
+      this.RSAPrivateKey = data["KeyMaterial"];
+      this.keyName = data["KeyName"];
+      console.log("[PROJECT DETAILS] Received RSA key: " + this.RSAPrivateKey);
+    });
+  }
 
   ngOnInit() { 
     this.createForm = new FormGroup({
       'logicalId': new FormControl(null, Validators.required),
       'instanceType': new FormControl(this.instanceTypes[0]),
-      'keyName': new FormControl(null),
+      'keyName': new FormControl(Math.random().toString(36).substring(2, 15), Validators.required),
       'machineImage': new FormControl(this.machineImages[0]),
       'userData': new FormControl(null)
     });    
@@ -42,7 +58,7 @@ export class Ec2Component implements OnInit {
     M.FormSelect.init(this.machineImageSelect.nativeElement, {});
     M.FormSelect.init(this.keyPairActionSelect.nativeElement, {});
     M.updateTextFields();    
-    this.initialFormValues = this.createForm.value;
+    this.initialFormValues = this.createForm.value;    
   }
 
   onFileChange(e) {
@@ -57,7 +73,8 @@ export class Ec2Component implements OnInit {
     };
   }
 
-  onSubmit() {    
+  onSubmit() {
+    this.isLoading = true; 
     console.log(this.createForm.value);
     this.create.emit(this.createForm.value);  
 
@@ -66,6 +83,20 @@ export class Ec2Component implements OnInit {
     // Reset controls not part of FormGroup manually
     this.createNewKeyPair = false;
     this.fileInput.nativeElement.value = "";
-    this.fileName.nativeElement.value = "";
+    this.fileName.nativeElement.value = "";        
+  }
+
+  onRSAPrivateKeyDownload() {
+    let blob = new Blob([this.RSAPrivateKey], { type: 'text/plain' });
+    var url = window.URL.createObjectURL(blob);
+    var anchor = document.createElement("a");
+    anchor.download = this.keyName+'.pem';
+    anchor.href = url;
+    anchor.click();
+
+    // Clear RSA private key data after download
+    this.receivedRSAPrivateKeyPair = false;
+    this.RSAPrivateKey = null;
+    this.keyName = null;
   }
 }
