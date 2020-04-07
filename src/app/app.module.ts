743d605544310f0a8cee3ba9c1d396109efee610
Cristian Gutu <gutucristian27@gmail.com>
2020-04-07 11:41:19 -0400

    [create-ec2-comp]: clean up service imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,7 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Ec2Component } from './ec2/ec2.component';
 import { CreateUserComponent } from './create-user/create-user.component';
-import { AWSClientService } from 'src/services/awsclient.service';
-import { NotificationService } from 'src/services/notification.service';
-import { LoggingService } from 'src/services/logger.service';
+import { AWSClientService, NotificationService, LoggingService } from 'src/services/services';
 
 @NgModule({
   declarations: [
