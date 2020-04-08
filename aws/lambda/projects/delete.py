d7dfb3f3c0f10f7fa4b44224a8aa0ad2b2ff458d
Tyler Cromwell <tjc6185@gmail.com>
2020-04-08 15:36:47 -0400

    [api]: Removed querying logic for EC2ResourcesTable

--- aws/lambda/projects/delete.py
+++ aws/lambda/projects/delete.py
@@ -29,32 +29,6 @@ def main(event, context):
         StackName=project['Item']['name']['S'].replace(' ', '')
     )
 
-    # Get the associated rows from "EC2ResourcesTable" table
-    response = dbclient.scan(
-        TableName='EC2ResourcesTable',
-        ExpressionAttributeNames={
-            '#id': 'id'
-        },
-        ExpressionAttributeValues={
-            ':id': {
-                'S': pid
-            }
-        },
-        FilterExpression='projectId = :id',
-        ProjectionExpression='#id'
-    )
-
-    # Delete the associated rows from "EC2ResourcesTable" table
-    for row in response['Items']:
-        response = dbclient.delete_item(
-            TableName='EC2ResourcesTable',
-            Key={
-                'id': {
-                    'S': row['id']['S']
-                }
-            }
-        )
-
     # Delete project row from "ProjectsTable" table
     response = dbclient.delete_item(
         TableName='ProjectsTable',
